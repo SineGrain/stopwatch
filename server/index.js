24e7d65f3e72eea28110bb98da8e6c2fd9cfc980
Stefano <stefano.vinciarelli@immobiliare.it>
2023-05-19 19:25:36 +0200

    feat: added prod mode to server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,12 @@ var https = require("https");
 var fs = require("fs");
 const express = require('express');
 const app = express();
-// const http = require('http');
+const DECK = require("../shared/constants/deck")
+const dev = require('./dev')
+
+const DEV = false;
+const PORT = DEV ? 4000 : 443
+
 const server = https
   .createServer(
     {
@@ -12,20 +17,13 @@ const server = https
     app
   );
 const { Server } = require("socket.io");
-const { join } = require('path')
-const readline = require('readline');
-const DECK = require("../shared/constants/deck")
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:3000"
     }
 });
 
-app.get(['/', '/control'], (req, res) => {
-    res.sendFile(join(__dirname, '../client/build/index.html'));
-});
 
-app.use('/stopwatch', express.static(join(__dirname, '../client/build')))
 
 io.on('connection', (socket) => {
     console.log('a user connected');
@@ -40,43 +38,10 @@ io.on('connection', (socket) => {
 
 
 
-server.listen(4000, () => {
-    console.log('listening on *:4000');
+server.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
 });
 
-
-readline.emitKeypressEvents(process.stdin);
-if (process.stdin.isTTY) process.stdin.setRawMode(true);
-
-// listen to keypress
-let comb = ['', '']
-let combIndex = true
-process.stdin.on("keypress", (str, key) => {
-    if (key.ctrl && key.name == "s") console.log("save")
-
-    if (key.ctrl && key.name == "c") process.exit()
-    console.log('key')
-    if (key.name === 'space') {
-        comb[0] = ''
-        comb[1] = ''
-    } else {
-        if (combIndex) {
-            comb[0] = key.name
-            combIndex = false
-        } else {
-            comb[1] = key.name
-            combIndex = true
-        }
-    }
-    const cardMaybe = `${comb[0].toLowerCase()}${comb[1].toLowerCase()}`
-    console.log('cardMaybe', cardMaybe)
-    if (DECK.findIndex(itm => itm === cardMaybe) > -1) {
-        console.log(`emitting ${cardMaybe}`)
-        io.emit('selectedCard', cardMaybe)
-        comb[0] = ''
-        comb[1] = ''
-        combIndex = true
-    }
-
-
-})
+if(DEV){
+    dev(app, io);
+}
